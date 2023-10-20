97ce385273fbe0ca2535a8d30659f735bd1f14a0
Callum Moss <108734854+AceMegalodon@users.noreply.github.com>
2023-10-20 23:42:01 +0100

    Update results_evaluator.py

--- results_evaluator.py
+++ results_evaluator.py
@@ -1,8 +1,14 @@
 """ Compare poisoned to see if its a superset """
 # Checks whether the same functions that are expected to be poisoned are still poisoned or if they have changed
+import os
 
-with open("expected.txt", 'r') as expected_output_file:
-    with open("output.txt", 'r') as output_file:
+user_name = os.getenv("USER")
+if user_name is None:
+    user_name = os.getenv("LOGNAME")
+    
+with open("/home/" + user_name + "/Mitigating_Backdoor_Attacks_in_LLMs/adversarial-backdoor-for-code-models/CodeT5/sh/saved_models/summarize-adv-0.05/python/codet5_small_all_lr5_bs32_src256_trg128_pat2_e15/prediction/dev_e0.gold", 'r') as expected_output_file:
+    
+    with open("/home/" + user_name + "/Mitigating_Backdoor_Attacks_in_LLMs/adversarial-backdoor-for-code-models/CodeT5/sh/saved_models/summarize-adv-0.05/python/codet5_small_all_lr5_bs32_src256_trg128_pat2_e15/prediction/dev_e0.output", 'r') as output_file:
         expected_output = expected_output_file.readlines()# if different, print
         output = output_file.readlines()
         count = 0
@@ -12,7 +18,8 @@ with open("expected.txt", 'r') as expected_output_file:
                 count2 += 1
                 if expected_line != output[index]:
                     count +=1
-                    print("Output: " + output[index])
-                    print("Expected Output: " + expected_line + "\n")
-        print(count)
-        print(count2)
+                    print("Successful shift on this element: " + output[index])
+                    #print("Expected Output: " + expected_line + "\n")
+                    
+        print("Number of successful shifts: " + str(count))
+        print("Number of poisoned elements: " + str(count2))
